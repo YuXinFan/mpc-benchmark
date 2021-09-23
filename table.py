748314d643893f6fcda924f7b6a42506e89ab658
fanyx <fanyx@shanghaitech.edu.cn>
2021-09-23 16:57:26 +0800

    add csv table py script

--- table.py
+++ table.py
@@ -1,8 +1,18 @@
 import pandas as pd 
 
-p0=pd.read_csv("Party-0.csv", header=0,names=['Name', 'InSize', 'Type', 'Val', "Repeat"])
-p1=pd.read_csv("Party-1.csv", header=0)
+p0=pd.read_csv("Party-0.csv", names=['Name', 'InSize', 'Type', 'Val', "Repeat"])
+p1=pd.read_csv("Party-1.csv", names=['Name', 'InSize', 'Type', 'Val', "Repeat"])
 print(p0)
-
-for i in range(len(p0["Val"])):
-    p0["Val"][i] = max(p0["Val"][i],p1["Val"][i])
+pp ={"Name": p0["Name"], "InSize": p0["InSize"],"Type": p0["Type"]}
+val = []
+l0 = p0["Val"].to_list()
+l1 = p1["Val"].to_list()
+assert(len(l0)==len(l1))
+for i in range(len(l0)):
+    v1 = l0[i]
+    v2 = l1[i]
+    val.append(max(v1,v2))
+pp["Val"] = val 
+pp["Repeat"] = p0["Repeat"]
+df = pd.DataFrame(pp)
+df.to_csv("./Party.csv", index=False)
