decc829c63a9bd1534604e68af84b91ed7f8f9d1
fanyx <fanyx@shanghaitech.edu.cn>
2021-09-24 21:40:31 +0800

    change mpyc input gen

--- mpyc/almost_search.py
+++ mpyc/almost_search.py
@@ -1,156 +1,150 @@
 from mpyc.runtime import mpc    # load MPyC
+from mpyc.seclists import seclist, secindex
 secint = mpc.SecInt()           # 32-bit secure MPyC integers
 mpc.run(mpc.start())            # required only when run with multiple parties
 import traceback     
 import math  
 
-@mpc.coroutine
-async def binary_almost_search_main(needle, arr, l, r):
-    if (r >= l):
-        mid = l + int((r-l)/2)
-        left = mid > l
-        if left :
-            left = arr[mid-1] == needle
-        right = mid < r 
-        if right:
-            right = arr[mid+1] == needle 
+def gen_sorted_array(n):
+    import random 
+    l = []
+    v = 0
+    for i in range(n):
+        v = v + random.randrange(0,8)
+        l.append(v)
 
-        oeq = arr[mid] == needle 
-
-        idx = mpc.if_else(oeq, mid, -1)
-        idx = mpc.if_else(left, (mid-1), mpc.if_else(right, mid+1, idx))
-        
-        gt = arr[mid] > needle 
-        lt = arr[mid] < needle 
-        if mid-2 >= l :
-            ll = binary_almost_search_opt_main(needle, arr, l, mid - 2)
-            idx = mpc.if_else(gt, ll, idx)
-        if r >= mid+2:
-            rr = binary_almost_search_opt_main(needle, arr, mid + 2, r)
-            idx = mpc.if_else(lt, rr, idx)
-        return idx 
-
-def binary_almost_search(needle, arr, n):
-    return binary_almost_search_opt_main(needle, arr, 0, n-1)
+    return l 
 
 @mpc.coroutine
-async def binary_almost_search_opt_main(needle, arr, l, r):
-    if (r >= l):
-        mid = l + int((r-l)/2)
-        left = mid > l
-        if left :
-            left = arr[mid-1] == needle
-        right = mid < r 
-        if right:
-            right = arr[mid+1] == needle 
-        eq = mpc.eq_public(arr[mid], needle)
+async def binary_almost_search_main(haystack, haystack_length, needle):
+    upper_bound = int(math.log2(haystack_length)) + 1
+    index = secint(-1)
+    iimin = secint(0)
+    iimax = secint(haystack_length - 1)
+    bb = needle
+    for _ in range(upper_bound):
+        ii = iimin + iimax
+        cc =  ii % 2
+        iimid = mpc.if_else( cc, mpc.div(iimax+iimin-1, 2), mpc.div(iimax+iimin, 2) )
+        aa = haystack[iimid] # oram_read(haystack, haystack_length, iimid)
+        oeq = aa == bb 
+        index = mpc.if_else(oeq, iimid, index)
+        left = haystack[iimid-1]
+        index = mpc.if_else(aa==left, iimid-1, index)
 
-        if await (eq):
-            return mid 
-        else:
-            idx = mpc.if_else(left, (mid-1), mpc.if_else(right, mid+1, -1))
-            
-            gt = arr[mid] > needle 
-            lt = arr[mid] < needle 
-            if mid-2 >= l :
-                ll = binary_almost_search_opt_main(needle, arr, l, mid - 2)
-                idx = mpc.if_else(gt, ll, idx)
-            if r >= mid+2:
-                rr = binary_almost_search_opt_main(needle, arr, mid + 2, r)
-                idx = mpc.if_else(lt, rr, idx)
-            return idx 
+        right = haystack[iimid+1]
+        index = mpc.if_else(aa==right, iimid+1, index)
 
-def binary_almost_search_opt(needle, arr, n):
-    return binary_almost_search_opt_main(needle, arr, 0, n-1)
+        ogt = aa > bb
+        iimin = mpc.if_else(ogt, iimin, iimid+2)
+        iimax = mpc.if_else(ogt, iimid-2, iimax)
+    return index
 
 
+def binary_almost_search(arr, n, needle):
+    return binary_almost_search_main(arr, n, needle)
 
-def testOpt(samples=1, n=10):
+@mpc.coroutine
+async def binary_almost_search_opt_main(haystack, haystack_length, needle):
+    upper_bound = int(math.log2(haystack_length)) + 1
+    index = secint(-1)
+    iimin = secint(0)
+    iimax = secint(haystack_length - 1)
+    bb = needle
+    for _ in range(upper_bound):
+        ii = iimin + iimax
+        cc =  ii % 2
+        iimid = mpc.if_else( cc, mpc.div(iimax+iimin-1, 2), mpc.div(iimax+iimin, 2) )
+        aa = haystack[iimid] # oram_read(haystack, haystack_length, iimid)
+        eq = await mpc.eq_public(aa, bb)
+        if eq:
+            index = iimid 
+            break  
+        left = haystack[iimid-1]
+        index = mpc.if_else(aa==left, iimid-1, index)
+
+        right = haystack[iimid+1]
+        index = mpc.if_else(aa==right, iimid+1, index)
+
+        ogt = aa > bb
+        iimin = mpc.if_else(ogt, iimin, iimid+2)
+        iimax = mpc.if_else(ogt, iimid-2, iimax)
+    return index
+
+def binary_almost_search_opt(arr, n, needle):
+    return binary_almost_search_opt_main(arr, n, needle)
+
+
+
+def testOpt(arraySize=10, searchSize = 1, samples=1):
     import random
     import time
-    MIN = 0
-    repeat = n
-    MAX = 2*n 
 
     mpc.run(mpc.start())            # required only when run with multiple parties
 
     totalTime = 0
     print("start benchmark binary search opt, %d times repeat:" % samples)
     for kk in range(samples):
-        cleartext = random.sample(range(MIN, MAX), n)
-        cleartext = sorted(cleartext)
+        cleartext = gen_sorted_array(arraySize)
+        eles = [cleartext[random.randint(0,arraySize-1)] for _ in range(arraySize)]
         x = list(map(secint, cleartext))
-        for jj in range(repeat):
-            ele = random.randrange(MIN,MAX)
-            e = secint(ele)
+        x = seclist(x)
+        for jj in range(searchSize):
+            e = secint(eles[jj])
             # print(x)
             timeS = time.perf_counter()
-            oy = binary_almost_search_opt(e, x, n)
+            oy = binary_almost_search_opt(x, arraySize, e)
             #y=mpc.run(mpc.output(oy))
             timeE = time.perf_counter()
-
-            # if (y != -1):
-            #     print("In:",cleartext[y]==ele)
-            # else:
-            #     print("Not-in:", cleartext.count(ele)==0)
 
             timeDiff = timeE-timeS 
             #print("%.3f, " % timeDiff, end="")
             totalTime += timeDiff
-    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples/repeat))
-
+    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples))
     mpc.run(mpc.shutdown())
 
-def testRaw(samples=1, n=10):
+def testRaw(arraySize=10, searchSize = 1, samples=1):
     import random
     import time
-    
-    MIN = 0
-    repeat = n
-    MAX = 2*n 
 
     mpc.run(mpc.start())            # required only when run with multiple parties
 
     totalTime = 0
     print("start benchmark binary search raw, %d times repeat:" % samples)
     for kk in range(samples):
-        cleartext = random.sample(range(MIN, MAX), n)
-        cleartext = sorted(cleartext)
+        cleartext = gen_sorted_array(arraySize)
+        eles = [cleartext[random.randint(0,arraySize-1)] for _ in range(arraySize)]
         x = list(map(secint, cleartext))
-        for jj in range(repeat):
-            ele = random.randrange(MIN,MAX)
-            e = secint(ele)
+        x = seclist(x)
+        for jj in range(searchSize):
+            e = secint(eles[jj])
             # print(x)
             timeS = time.perf_counter()
-            oy = binary_almost_search(e, x, n)
+            oy = binary_almost_search(x, arraySize, e)
             #y=mpc.run(mpc.output(oy))
             timeE = time.perf_counter()
-
-            # if (y != -1):
-            #     print("In:",cleartext[y]==ele)
-            # else:
-            #     print("Not-in:", cleartext.count(ele)==0)
 
             timeDiff = timeE-timeS 
             #print("%.3f, " % timeDiff, end="")
             totalTime += timeDiff
-    print()
-    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples/repeat))
+    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples))
     mpc.run(mpc.shutdown())
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--opt', action='store_true')
-    parser.add_argument('-n', type=int)
-    parser.add_argument('-r', type=int)
+    parser.add_argument('-e', type=int)
+    parser.add_argument('-s', type=int)
+    parser.add_argument('-i', type=int)
+
 
     args = parser.parse_args()
 
     if args.opt == True:
-        testOpt(args.r, args.n)
+        testOpt(args.e, args.s, args.i)
     else:
-        testRaw(args.r, args.n)
+        testRaw(args.e, args.s, args.i)
 main()
 
 
--- mpyc/binary_search.py
+++ mpyc/binary_search.py
@@ -3,12 +3,16 @@ from mpyc.seclists import seclist, secindex
 secint = mpc.SecInt()           # 32-bit secure MPyC integers
 import traceback     
 import math  
+def gen_sorted_array(n):
+    import random 
+    l = []
+    v = 0
+    for i in range(n):
+        v = v + random.randrange(0,8)
+        l.append(v)
 
-def oram_read(arr, size, idx):
-    ret = secint(-1)
-    for i in range(size):
-        ret = mpc.if_else(i == idx, arr[i], ret)
-    return ret 
+    return l 
+
 
 @mpc.coroutine
 async def obinary_search(haystack, haystack_length, needle):
@@ -17,7 +21,6 @@ async def obinary_search(haystack, haystack_length, needle):
     iimin = secint(0)
     iimax = secint(haystack_length - 1)
     bb = needle
-    haystack = seclist(haystack, secint)
     for _ in range(upper_bound):
         ii = iimin + iimax
         cc =  ii % 2
@@ -38,7 +41,6 @@ async def obinary_search_opt(haystack, haystack_length, needle):
     iimin = secint(0)
     iimax = secint(haystack_length - 1)
     bb = needle
-    haystack = seclist(haystack, secint)
     for _ in range(upper_bound):
         ii = iimin + iimax
         cc =  ii % 2
@@ -55,90 +57,72 @@ async def obinary_search_opt(haystack, haystack_length, needle):
     return index
 
 
-def testOpt(samples=1, n=10):
+def testOpt(arraySize=10, searchSize = 1, samples=1):
     import random
     import time
-    MIN = 0
-    repeat = n
-    MAX = 2*n 
 
     mpc.run(mpc.start())            # required only when run with multiple parties
 
     totalTime = 0
     print("start benchmark binary search opt, %d times repeat:" % samples)
     for kk in range(samples):
-        cleartext = random.sample(range(MIN, MAX), n)
-        cleartext = sorted(cleartext)
+        cleartext = gen_sorted_array(arraySize)
+        eles = [cleartext[random.randint(0,arraySize-1)] for _ in range(arraySize)]
         x = list(map(secint, cleartext))
-        for jj in range(repeat):
-            ele = random.randrange(MIN,MAX)
-            e = secint(ele)
+        x = seclist(x)
+        for jj in range(searchSize):
+            e = secint(eles[jj])
             # print(x)
             timeS = time.perf_counter()
-            oy = obinary_search_opt(x, n, e)
+            oy = obinary_search_opt(x, arraySize, e)
             #y=mpc.run(mpc.output(oy))
             timeE = time.perf_counter()
-
-            # if (y != -1):
-            #     print("In:",cleartext[y]==ele)
-            # else:
-            #     print("Not-in:", cleartext.count(ele)==0)
 
             timeDiff = timeE-timeS 
             #print("%.3f, " % timeDiff, end="")
             totalTime += timeDiff
-    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples/repeat))
-
+    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples))
     mpc.run(mpc.shutdown())
 
-def testRaw(samples=1, n=10):
+def testRaw(arraySize=10, searchSize = 1, samples=1):
     import random
     import time
-    
-    MIN = 0
-    repeat = n
-    MAX = 2*n 
 
     mpc.run(mpc.start())            # required only when run with multiple parties
 
     totalTime = 0
-    print("start benchmark binary search raw, %d times repeat:" % samples)
+    print("start benchmark binary search opt, %d times repeat:" % samples)
     for kk in range(samples):
-        cleartext = random.sample(range(MIN, MAX), n)
-        cleartext = sorted(cleartext)
+        cleartext = gen_sorted_array(arraySize)
+        eles = [cleartext[random.randint(0,arraySize-1)] for _ in range(arraySize)]
         x = list(map(secint, cleartext))
-        for jj in range(repeat):
-            ele = random.randrange(MIN,MAX)
-            e = secint(ele)
+        x = seclist(x)
+        for jj in range(searchSize):
+            e = secint(eles[jj])
             # print(x)
             timeS = time.perf_counter()
-            oy = obinary_search(x, n, e)
+            oy = obinary_search(x, arraySize, e)
             #y=mpc.run(mpc.output(oy))
             timeE = time.perf_counter()
-
-            # if (y != -1):
-            #     print("In:",cleartext[y]==ele)
-            # else:
-            #     print("Not-in:", cleartext.count(ele)==0)
 
             timeDiff = timeE-timeS 
             #print("%.3f, " % timeDiff, end="")
             totalTime += timeDiff
-    print()
-    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples/repeat))
+    print("Total repeat %d times. Average execution time is %.3fs." % (samples, totalTime/samples))
     mpc.run(mpc.shutdown())
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--opt', action='store_true')
-    parser.add_argument('-n', type=int)
-    parser.add_argument('-r', type=int)
+    parser.add_argument('-e', type=int)
+    parser.add_argument('-s', type=int)
+    parser.add_argument('-i', type=int)
 
     args = parser.parse_args()
 
     if args.opt == True:
-        testOpt(args.r, args.n)
+        testOpt(args.e, args.s, args.i)
     else:
-        testRaw(args.r, args.n)
+        testRaw(args.e, args.s, args.i)
 main()
