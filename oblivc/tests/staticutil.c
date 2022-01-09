5603eaca200a42340ee371728ad0b2f2812ff010
fanyx <fanyx@shanghaitech.edu.cn>
2022-01-09 17:22:50 +0800

    LinS v1 v2

--- oblivc/tests/staticutil.c
+++ oblivc/tests/staticutil.c
@@ -14,7 +14,7 @@ void static_write_hard(int party, char *name, int arrSize, char *type, float val
       printf("[static_write]:Open File Error!");   
       exit(1);             
    }
-   fprintf(fptr,"%s,%d, %s, %.3f, %d\n",name, arrSize, type, val, repeat);
+   fprintf(fptr,"%s,%d, %s, %.6f, %d\n",name, arrSize, type, val, repeat);
 
    fclose(fptr);
 }
@@ -34,7 +34,7 @@ void static_write_simple(int party, char *name, int eleSize, int arrSize, char *
       printf("[static_write]:Open File Error!");   
       exit(1);             
    }
-   fprintf(fptr,"%s,%d/%d, %s, %.3f, %d\n",name, eleSize, arrSize, type, val, repeat);
+   fprintf(fptr,"%s,%d/%d, %s, %.6f, %d\n",name, eleSize, arrSize, type, val, repeat);
 
    fclose(fptr);
 }
